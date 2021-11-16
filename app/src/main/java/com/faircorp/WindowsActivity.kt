c059a1a6d48dbdd76d71f8b052c4947a7557979f
Liwei Xu <liwei.xu@etu.emse.fr>
2021-11-17 00:56:15 +0100

    Local settings finished

--- app/src/main/java/com/faircorp/WindowsActivity.kt
+++ app/src/main/java/com/faircorp/WindowsActivity.kt
@@ -1,13 +1,16 @@
 package com.faircorp
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.faircorp.model.OnWindowSelectedListener
 import com.faircorp.model.WindowAdapter
 import com.faircorp.model.WindowService
 
-class WindowsActivity : BasicActivity() {
+class WindowsActivity : BasicActivity(), OnWindowSelectedListener {
 
     val windowService = WindowService() // (1)
 
@@ -16,7 +19,7 @@ class WindowsActivity : BasicActivity() {
         setContentView(R.layout.activity_windows)
 
         val recyclerView = findViewById<RecyclerView>(R.id.list_windows) // (2)
-        val adapter = WindowAdapter() // (3)
+        val adapter = WindowAdapter(this) // (3)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
@@ -24,5 +27,10 @@ class WindowsActivity : BasicActivity() {
         recyclerView.adapter = adapter
 
         adapter.update(windowService.findAll()) // (4)
+    }
+
+    override fun onWindowSelected(id: Long) {
+        val intent = Intent(this, WindowActivity::class.java).putExtra(WINDOW_NAME_PARAM, id)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/faircorp/model/WindowAdapter.kt
+++ app/src/main/java/com/faircorp/model/WindowAdapter.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.faircorp.R
 
-class WindowAdapter : RecyclerView.Adapter<WindowAdapter.WindowViewHolder>() { // (1)
+class WindowAdapter(val listener: OnWindowSelectedListener): RecyclerView.Adapter<WindowAdapter.WindowViewHolder>() {
 
     inner class WindowViewHolder(view: View) : RecyclerView.ViewHolder(view) { // (2)
         val name: TextView = view.findViewById(R.id.txt_window_name)
@@ -31,12 +31,22 @@ class WindowAdapter : RecyclerView.Adapter<WindowAdapter.WindowViewHolder>() { /
         return WindowViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: WindowViewHolder, position: Int) {  // (7)
+
+    override fun onBindViewHolder(holder: WindowViewHolder, position: Int) {
         val window = items[position]
         holder.apply {
             name.text = window.name
             status.text = window.status.toString()
             room.text = window.room.name
+            itemView.setOnClickListener { listener.onWindowSelected(window.id) } // (1)
         }
+    }
+
+    override fun onViewRecycled(holder: WindowViewHolder) { // (2)
+        super.onViewRecycled(holder)
+        holder.apply {
+            itemView.setOnClickListener(null)
+        }
+
     }
 }
